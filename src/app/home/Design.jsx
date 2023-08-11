317335a598e0ede0dd4bb8b852cb15b63573947e
Resalat islam <resalatislam5@gmail.com>
2023-08-11 21:01:48 +0600

    fixed design bg

--- src/app/home/Design.jsx
+++ src/app/home/Design.jsx
@@ -20,7 +20,7 @@ function Design({selected}) {
                             <div className="flex flex-col sm:flex-row gap-3 sm:gap-0 justify-between">
                                 <div className="flex gap-3">
                                     <h3 className="sm:text-xl text-black font-bold">Product Designer</h3>
-                                    <p className="bg-[#F4F5FA] text-sm flex items-center sm:px-1 rounded-full"><Image className='w-5' src={dot} alt="" /> Design</p>
+                                    <p className="bg-[#F4F5FA] text-sm flex items-center px-2 rounded-full"><Image className='w-5' src={dot} alt="" /> Design</p>
                                 </div>
                                 <div className="bg-[#F4F5FA] gap-2 w-52 flex items-center px-1 rounded-full">
                                     <Image className='rounded-full w-5' src={asutralia} alt="" />
